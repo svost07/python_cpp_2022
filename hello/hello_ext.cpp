35b2fdde59319e3da92910422326bf9e9c526cf7
jluuM2 <jose.luu@gmail.com>
2020-03-15 20:51:58 +0000

    add gitignore

---
+++ hello/hello_ext.cpp
@@ -0,0 +1,12 @@
+// hello_ext.cpp
+char const* greet() {
+  return "hello, world";
+}
+
+#include <boost/python.hpp>
+
+BOOST_PYTHON_MODULE(hello_ext)
+{
+  using namespace boost::python;
+  def("greet", greet);
+}
